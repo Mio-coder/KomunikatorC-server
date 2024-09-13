e06acfec19f7a886073721600f490757b93a0c36
cyber-wojtek <wojtek.dudek.pl@gmail.com>
2024-09-13 19:32:13 +0200

    .

--- komunikatorc/chat/forms.py
+++ komunikatorc/chat/forms.py
@@ -2,5 +2,10 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=100)
+    username = forms.CharField(label="username", max_length=100)
     password = forms.CharField(label="password", max_length=100, widget=forms.PasswordInput)
+
+class SignUpForm(forms.Form):
+    username         = forms.CharField(label="username", max_length=100)
+    password         = forms.CharField(label="password", max_length=100, widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label="confirm_password", max_length=100, widget=forms.PasswordInput)
--- komunikatorc/chat/views.py
+++ komunikatorc/chat/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, SignUpForm
+import json
 
 
-def login(request: HttpRequest):
+def login(request: HttpRequest) -> JsonResponse | HttpResponse:
     if request.method == "POST":
         # api
         form = LoginForm(request.POST)
@@ -15,3 +16,20 @@ def login(request: HttpRequest):
             return JsonResponse({"errors": form.errors.as_json()}, status=400)
     form = LoginForm()
     return render(request, "login.html", {"form": form})
+
+
+def signup(request: HttpRequest) -> JsonResponse | HttpResponse:
+    if request.method == "POST":
+        # api
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            confirm_password = form.cleaned_data["confirm_password"]
+            if password != confirm_password:
+                return JsonResponse({"errors": form.errors.as_json()}, status=400)
+            return JsonResponse({"session_id": "absb-hrqp-foae-tyui"}, status=200)
+        else:
+            return JsonResponse({"errors": form.errors.as_json()}, status=400)
+    form = SignUpForm()
+    return render(request, "signup.html", {"form": form})
